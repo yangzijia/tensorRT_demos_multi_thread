30af53603d6521692801a376899234f3c80ed605
zjyang <zjyang@engyne.net>
2020-01-09 14:42:14 +0800

    fix

--- multi_thread_detect.py
+++ multi_thread_detect.py
@@ -9,6 +9,19 @@ import argparse
 from threading import Thread, Lock
 from queue import Queue
 import cv2
+import os
+
+
+yolov3_settings = {
+        "model_path": os.path.join('models', 'classes3.trt'),
+        "anchors_path": os.path.join('classes3', 'train_anchors.txt'),
+        "classes_path": os.path.join('classes3', 'voc_labels.txt'),
+        "model_masks": [(6, 7, 8), (3, 4, 5), (0, 1, 2)],
+        "score": 0.3,         # 对象覆盖的阈值，[0,1]之间
+        "nms_threshold": 0.5,       # nms的阈值，[0,1]之间
+        "model_image_size": (416, 416),
+        "output_shapes": [(1, 24, 13, 13), (1, 24, 26, 26), (1, 24, 52, 52)]
+    }
 
 
 class MyThread(Thread):
@@ -31,9 +44,9 @@ class MyThread(Thread):
             try:
                 cuda_ctx = cuda.Device(0).make_context()
                 if self.mode == "yolov3":
-                    model = Yolov3TRT()
+                    model = Yolov3TRT(**yolov3_settings)
                 elif self.mode == "vgg16":
-                    model = VGG16(overall_model_path="vgg16.uff")
+                    model = VGG16(overall_model_path="vgg16.model.uff")
 
                 matrix = self.queue.get(block=True)
 
